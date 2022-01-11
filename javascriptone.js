3bc07aaa74215a5142dd575bbf59ecbca76661b4
nftmaax <maxescajadillopoma@gmail.com>
2022-01-11 08:07:12 -0800

    files that dont make sense but one day will

---
+++ javascriptone.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>websiteone</title>
+</head>
+<body>
+<div>
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+</div>
+<div>
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+</div>
+<div>
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+</div>
+
+</body>
+</html>
